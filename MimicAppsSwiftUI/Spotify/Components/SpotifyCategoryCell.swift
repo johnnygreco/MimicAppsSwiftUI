555c5926f86160e9a0d258022011277b71387f0b
Johnny Greco <jgreco.ai@gmail.com>
2024-08-17 15:08:38 -0400

    spotify skeleton

---
+++ MimicAppsSwiftUI/Spotify/Components/SpotifyCategoryCell.swift
@@ -0,0 +1,36 @@
+//
+//  SpotifyCategoryCell.swift
+//  MimicAppsSwiftUI
+//
+//  Created by Johnny Greco on 8/17/24.
+//
+
+import SwiftUI
+
+struct SpotifyCategoryCell: View {
+    
+    var title: String = "All"
+    var isSelected: Bool = false
+    
+    var body: some View {
+        Text(title)
+            .font(.callout)
+            .frame(minWidth: 35)
+            .padding(.vertical, 8)
+            .padding(.horizontal, 10)
+            .background(isSelected ? .spotifyGreen : .spotifyDarkGray)
+            .foregroundStyle(isSelected ? .spotifyBlack : .spotifyWhite)
+            .cornerRadius(16)
+    }
+}
+
+#Preview {
+    ZStack {
+        Color.black.ignoresSafeArea()
+        VStack(spacing: 40) {
+            SpotifyCategoryCell(title: "Title goes here")
+            SpotifyCategoryCell(title: "Title goes here", isSelected: true)
+            SpotifyCategoryCell(isSelected: true)
+        }
+    }
+}
---
+++ MimicAppsSwiftUI/Spotify/Models/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  MimicAppsSwiftUI
+//
+//  Created by Johnny Greco on 8/17/24.
+//
+
+import Foundation
+
+enum Category: String, CaseIterable {
+    case all, music, podcasts, audiobooks
+}
+
