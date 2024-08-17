b5e34e8b10f16e4602e2abc833a0b437b33c0aa1
Johnny Greco <jgreco.ai@gmail.com>
2024-08-17 11:48:08 -0400

    Initial Commit

---
+++ SwiftfulSwiftUIInPractice/SwiftfulSwiftUIInPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulSwiftUIInPracticeApp.swift
+//  SwiftfulSwiftUIInPractice
+//
+//  Created by Johnny Greco on 8/17/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulSwiftUIInPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
